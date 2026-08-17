95ba4d4e49c3618c0b366f393dbb6df61dc1513f
AdamJ2009 <adamdj2009@gmail.com>
2026-08-17 09:54:27 +0100

    readded cmd

--- lib/alces_sacct/parser.rb
+++ lib/alces_sacct/parser.rb
@@ -22,8 +22,8 @@ module AlcesSacct
         Elapsed Planned AllocCPUs TotalCPU ReqMem MaxRSS ExitCode
       ].join(',')
 
-      #cmd = "sacct -X #{user} -S #{flag[0]} -E #{flag[1]} #{partition} #{states} -P -n -o #{fields}|  tail -n 100000"
-      #parse_jobs(`#{cmd}`, raw_user)
+      cmd = "sacct -X #{user} -S #{flag[0]} -E #{flag[1]} #{partition} #{states} -P -n -o #{fields}|  tail -n 100000"
+      parse_jobs(`#{cmd}`, raw_user)
     end
 
     private
